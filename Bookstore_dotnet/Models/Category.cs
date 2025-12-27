cc2b69ff6b5a41dd4d38952b97776f813dc02982
Katelyn Ma <kama@ea.com>
2025-12-26 22:40:06 -0800

    Enhances category validation and adds success alert
    Adds max length and display order range validation to improve data integrity for categories.
    Displays success messages to users on the category list page for better feedback.
    Updates database file to reflect model validation changes.

--- Bookstore_dotnet/Models/Category.cs
+++ Bookstore_dotnet/Models/Category.cs
@@ -6,9 +6,13 @@ public class Category
 {
     [Key]
     public int Id { get; set; }
+
     [Required]
+    [MaxLength(30)]
     [Display(Name = "Category Name")]
     public string Name { get; set; }
+
+    [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100")]
     [Display(Name = "Display Order")]
     public int DisplayOrder { get; set; }
 }
